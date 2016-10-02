245fd48ed05f30060a037b5253b4f389a07d09f3
abhay <sabhayans@gmail.com>
2016-10-02 13:08:28 +0530

    cashier_dashboard

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,6 +18,10 @@ Route::get('home', 'HomeController@index');
 
 
 
+//cashier_Abhayan start//
+Route::get('cashier/dashboard', array('as' => 'get-dashboard', 'uses' => 'cashierController@getDashboard'));
+//cashier_Abhayan end//
+
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
