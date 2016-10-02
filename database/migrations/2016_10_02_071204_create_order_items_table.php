5f256556d58960cd70e914023afa7f7745f2bfdd
sanchi21 <ssanchi007@gmail.com>
2016-10-02 12:57:14 +0530

    Order, OrderItem model created

---
+++ database/migrations/2016_10_02_071204_create_order_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderItemsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('order_items', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->integer('order_id');
+            $table->string('item_id',20);
+            $table->integer('quantity');
+            $table->double('unit_price');
+            $table->integer('total');
+            $table->integer('discount');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('order_items');
+	}
+
+}
